bfe0e602ee7c4594cb0d267109e113ba7f146874
Jo Martins <joaovitor.11.2011@hotmail.com>
2025-03-17 14:58:05 -0300

    Adicionar arquivos de projeto.

---
+++ Jogo - Aula de terça/Form1.cs
@@ -0,0 +1,108 @@
+namespace Jogo___Aula_de_terça
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+
+
+        List<string> icons = new List<string>()
+        {
+            "!", "!", "N", "N", ",", ",", "K", "K",
+            "b", "b", "v", "v", "w", "w", "J", "J",
+        };
+
+        Label firstClicked = null;
+        Label secondClicked = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            AssignIconsToSquare();
+
+        }
+
+        private void AssignIconsToSquare()
+        {
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+                if (iconLabel != null)
+                {
+                    int randomNumber = random.Next(icons.Count);
+                    iconLabel.Text = icons[randomNumber];
+                    iconLabel.ForeColor = iconLabel.BackColor; // essa linha faz com que as imagens ficam escondidas
+                    icons.RemoveAt(randomNumber); // essa linha exclui o numero que ja foi selecionado, para que nao se repita mais de 2x
+                }
+            }
+        }
+
+        private void label_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled == true)
+            {
+                return;
+            }
+
+            Label clickedLabel = sender as Label;
+
+            if (clickedLabel != null)
+            {
+                if (clickedLabel.ForeColor == Color.Black)
+                {
+                    return;
+                }
+
+                if (firstClicked == null)
+                {
+                    firstClicked = clickedLabel;
+                    firstClicked.ForeColor = Color.Black;
+
+                    return;
+                }
+
+                secondClicked = clickedLabel;
+                secondClicked.ForeColor = Color.Black;
+
+                CheckForWinner();
+
+                if (firstClicked.Text == secondClicked.Text)
+                {
+                    firstClicked = null;
+                    secondClicked = null;
+                    return;
+                   
+                }
+
+                timer1.Start();
+            }
+        }
+
+        private void CheckForWinner()
+        {
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+
+                if (iconLabel != null)
+                {
+                    if (iconLabel.ForeColor == iconLabel.BackColor)
+                        return;
+                }
+            }
+
+            MessageBox.Show("Você combinou todos os pares, PARABÉNS");
+            Close();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+
+            firstClicked.ForeColor = firstClicked.BackColor;
+            secondClicked.ForeColor = secondClicked.BackColor;
+
+            firstClicked = null;
+            secondClicked = null;
+        }
+    }
+}
